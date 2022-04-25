5483a94d50b43351b4e9232606a61be68ffa908a
rolandpakai <roland.pakai@liferay.com>
2022-04-25 12:29:46 +0200

    Week1-Task3: Teach input to show a red outline for negative amounts.

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const MyInput = ({id, name, className, label}) => {
+
+    const isNegative = (num) => {
+        if (Math.sign(parseInt(num, 10)) === -1) {
+          return true;
+        }
+      
+        return false;
+    }
+
+    const validate = (element) => {
+        if (isNegative(element.value)) {
+            element.classList.add('invalid');
+        } else {
+            element.classList.remove('invalid');
+        }
+    }
+
     return (
         <>
         <label htmlFor={id}>
-            <input type="text" id={id} name={name} className={className} placeholder={label} />
+            <input 
+                type="number" 
+                id={id} 
+                name={name} 
+                className={className} 
+                placeholder={label} 
+                onChange={(e) => validate(e.target)}/>
             <span>{label}</span>
         </label>
         </>
