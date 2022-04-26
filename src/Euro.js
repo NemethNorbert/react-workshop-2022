322f73eb2bb81cc3e5f774a1425c034965e9d55d
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-26 18:29:40 +0200

    Added Red border

--- src/Euro.js
+++ src/Euro.js
@@ -1,9 +1,33 @@
-function Euro(props) {
+import PropTypes from 'prop-types';
+import React from 'react'
+
+import './Euro.css';
+
+function Euro({money}) {
+
+    const [borderClass, setBorder] = React.useState('black');
+    const [moneyValue, setValue] = React.useState({money})
     return (
         <div>
-            <input type="number" id="euro" name="euro" />
+            <input className={borderClass} type="number" id="euro" name="euro" value={moneyValue} onChange={handleChange}/>
         </div>
     )
+
+    function handleChange(event) {
+       console.log(event.target.value);
+       setValue(event.target.value)
+
+       if (event.target.value < 0) {
+            setBorder('red')
+       }
+       else {
+           setBorder('bleck')
+       }
+    }
+}
+
+Euro.propTypes = {
+    money: PropTypes.number,
 }
 
 export default Euro;
