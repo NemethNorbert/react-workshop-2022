46155563748cd1937074e2e64afa7d02a18785f7
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-26 10:36:25 +0200

    Added input with label ‘Euros’

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <label for="euro">Euros</label><div><input type="number" name="euro" id="euro" /></div>
     </div>
   );
 }
