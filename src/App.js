19a03a42d2659607d29561584c9348e49a61003b
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-26 20:27:26 +0200

    Added theme handling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React, { useContext }  from 'react';
 import './App.css';
 import Euro from './Euro.js';
 import BTC from './Btc.js';
 
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "lightblue"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "darkblue"
+  }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
 function App() {
+  const theme = useContext(ThemeContext);
   return (
-    <div className="App">
+    <ThemeContext.Provider value={themes.dark}>
+    <div className="App" style={{ background: theme.background }}>
       <label htmlFor="euro">Euros</label>
-      <Euro id="euro" money={0} />
+      <Euro id="euro" money={1000} />
       <label htmlFor="bitcoin">$BTC</label>
       <BTC id="bitcoin" btc={1000} />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
