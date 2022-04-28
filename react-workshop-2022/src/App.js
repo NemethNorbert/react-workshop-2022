e4bd279a12554278fc7b432e655126119fd89f2b
Kristóf Loneck <kristof.loneck@liferay.com>
2022-04-28 20:36:57 +0200

    feat(step 2): Create amount component

--- react-workshop-2022/src/App.js
+++ react-workshop-2022/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
 
-
 function App() {
   const [num, setNum] = useState(0);
+  const handleNumberSet = (value) => setNum(value)
+  return <Amount currencyType="Euros" value ={num} setValue = {handleNumberSet} />
+}
+
+function Amount(props) {
   return (
     <div className="App">
       <header className="App-header">
-      <input label = "Euros" type="number" value={num} onChange={event => setNum(event.target.value)}/>
+      <input label = {props.currencyType} type="number" value={props.value} onChange={(event) => props.setValue(event.target.value)}/>
       </header>
     </div>
   );
 }
+
 
 export default App;
