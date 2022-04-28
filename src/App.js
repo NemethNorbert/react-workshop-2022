88fb5e5a0c85785df53a1272d010742ddd794fb7
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-28 22:26:21 +0200

    Added number EUR input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useContext }  from 'react';
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
+      <label htmlFor="euro">Euros : </label>
+      <input type="number" id="euro"/>
     </div>
   );
 }
