0e5badce248616cdde31c4e09ae11918830887a9
Laszlo Pap <laszlo.pap@liferay.com>
2022-04-28 19:20:06 +0200

    Teach <Amount /> input to show a red outline for negative amounts., Make <Amount /> a controlled component (ie. pass it its value as a prop).

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import './App.css';
+import React from 'react';
 
+const defaultEuroValue=10;
+var isRedBorderAdded = false;
 
-const euroComponent = <Amount label="Euros"/>
 
 function App() {
+
+	const [mystate, setMyState] = React.useState({euroValue: defaultEuroValue});
+    const {euroValue} = mystate;
+
+    const myOnChange = (element) => {
+		let calcEur = element.value;
+
+		setMyState({
+		  euroValue: calcEur
+		});
+	}
+
+	const euroComponent = <Amount label="Euros" value={euroValue} onChange={myOnChange} />
+
   return (
     euroComponent
   );
 }
 
-function Amount({label}) {
+
+function Amount({label, value, onChange}) {
+
+	if (value >= 0){
+    	isRedBorderAdded = false;
+    } else {
+    	isRedBorderAdded = true;
+    }
+
 	return (
-    <div className="App">
+    <div className={isRedBorderAdded ? "error" : "App"}>
     	<label>{label}:
-        <input type="number" defaultValue={10}>
+         <input type="number" value={value} onChange=
+	        {
+	        	(event) => {
+	        		onChange(event.target)
+	        	}
+	        }
+        >
         </input>
       </label>
     </div>
