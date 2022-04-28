b193dd65dac8fc25619230e6251499205963281c
balazssk <balazs.safrany-kovalik@liferay.com>
2022-04-28 20:34:32 +0200

    WEEK-1 3. Teach <Amount /> input to show a red outline for negative amounts.

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,10 +1,26 @@
-const Amount = ({id, name}) => {
-	return (
-		<div>
-			<label htmlFor={id}>{name}</label>
-			<input id={id} type="number" />
-		</div>
-	)
+import './Amount.css';
+import React from 'react';
+
+class Amount extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {className: ""};
+	}
+	
+	handleChange = (event) => {
+		this.setState((state, props) => ({
+			className: event.target.value < 0 ? "red" : ""
+		}));
+	}
+	
+	render() {
+		return (
+			<div>
+				<label htmlFor={this.props.id}>{this.props.name}</label>
+				<input id={this.props.id} className={this.state.className} type="number" onChange={this.handleChange} />
+			</div>
+		);
+	}
 }
 
-export default Amount
+export default Amount;
