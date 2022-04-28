a7db2934e9ba06c577e4a78d79c981f3434bdd1a
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-29 00:23:47 +0200

    Added theming

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { useContext }  from 'react';
 import './App.css';
 
 
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "lightblue"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "darkblue"
+  }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
 function Currency ({id, name, value, readOnly, onChange}) {
   return (
+    
     <div>
       <label htmlFor={id}>{name}</label>
       <input id={id} type="number" value={value}  readOnly={readOnly} onChange={(e) => onChange(e.target)}/>
     </div>
+    
   )
 }
 
@@ -18,10 +33,13 @@ function exchangeRate() {
 function App() {
   const [value, setValue] = React.useState(0)
   const [btc, setBTC] = React.useState(0)
-
+  const [theme,setTheme] = React.useState(useContext(ThemeContext))
 
   let timeoutHanlder
 
+  function swapTheme(){
+    theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark)
+  }
   const validateValue = element => {
     if (element.value < 0) {
       element.className = 'red'
@@ -42,10 +60,15 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Currency id="euro" name="Euro" value={value} className="black" readOnly={false} onChange={validateValue}/>
-      <Currency id="btc" name="BTC" value={btc} className="black" readOnly={true}/>
-    </div>
+      <ThemeContext.Provider value={theme}>
+      <div className="App" style={{ background: theme.background, color: theme.foreground }}>
+        
+          <Currency id="euro" name="Euro" value={value} className="black" readOnly={false} onChange={validateValue}/>
+          <Currency id="btc" name="BTC" value={btc} className="black" readOnly={true}/>
+          <button value="Swap Theme" onClick={swapTheme} >Swap Theme</button>
+      </div>
+      </ThemeContext.Provider>
+      
   );
 }
 
