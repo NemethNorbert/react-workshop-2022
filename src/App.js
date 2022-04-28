81bcb1a5ff3c44a8d0a7fbe569ec5953bef14275
Laszlo Pap <laszlo.pap@liferay.com>
2022-04-28 19:42:58 +0200

    Add a second, read-only <Amount /> component that shows  instead of Euros

--- src/App.js
+++ src/App.js
@@ -7,26 +7,30 @@ var isRedBorderAdded = false;
 
 function App() {
 
-	const [mystate, setMyState] = React.useState({euroValue: defaultEuroValue});
-    const {euroValue} = mystate;
+	const [mystate, setMyState] = React.useState({euroValue: defaultEuroValue, btcValue: 0});
+    const {euroValue, btcValue} = mystate;
+    const exchangeRate = getExchangeRate();
 
     const myOnChange = (element) => {
 		let calcEur = element.value;
+		let calcBtc = element.value / exchangeRate;
 
 		setMyState({
-		  euroValue: calcEur
+		  euroValue: calcEur,
+		  btcValue: calcBtc
 		});
 	}
 
-	const euroComponent = <Amount label="Euros" value={euroValue} onChange={myOnChange} />
+	const euroComponent = <Amount label="Euros" value={euroValue} onChange={myOnChange} isReadOnly={false} />
+	const btcComponent = <Amount label="BTC" value={btcValue} isReadOnly={true} />
 
   return (
-    euroComponent
+    React.createElement(React.Fragment,null,euroComponent,btcComponent)
   );
 }
 
 
-function Amount({label, value, onChange}) {
+function Amount({label, value, onChange, isReadOnly}) {
 
 	if (value >= 0){
     	isRedBorderAdded = false;
@@ -35,9 +39,9 @@ function Amount({label, value, onChange}) {
     }
 
 	return (
-    <div className={isRedBorderAdded ? "error" : "App"}>
+    <div className={(isRedBorderAdded && !isReadOnly) ? "error" : "App"}>
     	<label>{label}:
-         <input type="number" value={value} onChange=
+         <input type="number" value={value} readOnly={isReadOnly} onChange=
 	        {
 	        	(event) => {
 	        		onChange(event.target)
@@ -48,6 +52,10 @@ function Amount({label, value, onChange}) {
       </label>
     </div>
   );
+}
+
+function getExchangeRate() {
+	return Math.random() * 10000;
 }
 
 export default App;
