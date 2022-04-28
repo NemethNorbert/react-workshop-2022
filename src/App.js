28695ecf40f846c2b34afd09ce4b21d925d6bdd5
Laszlo Pap <laszlo.pap@liferay.com>
2022-04-28 18:59:26 +0200

    Extract the input into a separate component called 'Amount' that takes a name (eg. Euros) prop.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+
+const euroComponent = <Amount label="Euros"/>
+
 function App() {
   return (
+    euroComponent
+  );
+}
+
+function Amount({label}) {
+	return (
     <div className="App">
-	    <label>Euros:
-	        <input type="number" defaultValue={10} />
+    	<label>{label}:
+        <input type="number" defaultValue={10}>
+        </input>
       </label>
     </div>
   );
