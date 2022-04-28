30d7cf8b83036339e43c201cfee13226f083250c
Kristóf Loneck <kristof.loneck@liferay.com>
2022-04-29 01:05:51 +0200

    feat(step 5): btc dollar added

--- react-workshop-2022/src/App.js
+++ react-workshop-2022/src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
 import React, { useState } from 'react';
+import './Amount.css';
+
+
 
 
 function App() {
   const [num, setNum] = useState(0);
-  const handleNumberSet = (value) => setNum(value)
-  return <Amount currencyType="Euros" value ={num} setValue = {handleNumberSet} />
+  return <div className="App">
+          <header className="App-header">
+            <Amount currencyType="Euros" value ={num} setValue = {setNum} />
+            <BtwAmount value ={num} setValue = {setNum} /> 
+          </header>
+        </div>;
 }
 
 function Amount(props) {
+  const exchangeRate = props.exchangeRate ? props.exchangeRate : 1;
+  const className = props.value < 0 ? "Wrong-amount" : "";
   return (
-    <div className="App">
-      <header className="App-header">
-      <input label = {props.currencyType} type="number" value={props.value} onChange={(event) => props.setValue(event.target.value)}/>
-      </header>
-    </div>
-  );
+      <input className={className} 
+      label = {props.currencyType} 
+      type="number" 
+      value={exchangeRate*props.value} 
+      onChange={(event) => props.setValue(event.target.value*exchangeRate)}/>
+  )
+}
+
+function BtwAmount(props) {
+  const [exchangeRate, setExchangeRate] = useState(Math.random() * 10000)
+  return (
+      <Amount {...props} exchangeRate = {exchangeRate} currencyType="$BTC"/>
+  )
 }
 
 
