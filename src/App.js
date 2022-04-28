01acdebc522bbc8e1694c5b6b6a876f5988b8508
Vilmos Papp <vilmos.papp@liferay.com>
2022-04-28 23:41:01 +0200

    Teach input to show red oufit for negative number

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { useContext }  from 'react';
 import './App.css';
 
 
-function Currency ({id, name, className, value, readOnly}) {
+function Currency ({id, name, value, readOnly}) {
+  
+  const validateValue = event => {
+    if (event.target.value < 0) {
+      event.target.className = 'red'
+    }
+    else {
+      event.target.className = 'black'
+    }
+  }
+
   return (
     <div>
       <label htmlFor={id}>{name}</label>
-      <input className={className} id={id} type="number" value={value}  readOnly={readOnly} />
+      <input id={id} type="number" value={value}  readOnly={readOnly} onChange={validateValue}/>
     </div>
   )
 }
 
 function App() {
-  
+  const handleChange = event => {
+    console.log('handle change')
+  }
+
   return (
     <div className="App">
-      <Currency id="euro" name="Euro" value="0" className="black" readOnly={false} />
+      <Currency id="euro" name="Euro"  className="black" readOnly={false} onChange={handleChange} />
+      <Currency id="btc" name="BTC" defaultValue="0" className="black" readOnly={true}/>
     </div>
   );
 }
