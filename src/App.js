ff58771deb78075ef620849a0b7931cdb115dfcf
balazssk <balazs.safrany-kovalik@liferay.com>
2022-04-28 20:54:56 +0200

    WEEK-1 5. Add a second, read-only <Amount /> component that shows  instead of Euros; use this function to get the exchange rate:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Amount from './components/Amount';
+import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-		<Amount id="euros" name="Euros" value="2" />
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	
+	exchangeRate() {
+      return Math.random() * 10000;
+    }
+  
+	render() {
+		return (
+			<div className="App">
+				<Amount id="euros" name="Euros" value="2" />
+				<Amount id="btc" name="$BTC" readOnly={true} value={this.exchangeRate()} />
+			</div>
+		);
+	}
 }
 
 export default App;
