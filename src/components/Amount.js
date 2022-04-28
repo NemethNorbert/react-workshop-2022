a2ee178426c485e2c871b75bab30a643454e627a
balazssk <balazs.safrany-kovalik@liferay.com>
2022-04-28 20:45:48 +0200

    WEEK-1 4. Make <Amount /> a controlled component (ie. pass it its value as a prop).

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -4,20 +4,32 @@ import React from 'react';
 class Amount extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {className: ""};
+		this.state = {
+			className: "",
+			value : this.props.value
+		};
 	}
 	
 	handleChange = (event) => {
 		this.setState((state, props) => ({
-			className: event.target.value < 0 ? "red" : ""
+			className: event.target.value < 0 ? "red" : "",
+			value: event.target.value
 		}));
 	}
 	
 	render() {
 		return (
 			<div>
-				<label htmlFor={this.props.id}>{this.props.name}</label>
-				<input id={this.props.id} className={this.state.className} type="number" onChange={this.handleChange} />
+				<label htmlFor={this.props.id}>
+					{this.props.name}
+				</label>
+				<input
+					id={this.props.id}
+					className={this.state.className}
+					type="number"
+					onChange={this.handleChange}
+					value={this.state.value}
+				/>
 			</div>
 		);
 	}
