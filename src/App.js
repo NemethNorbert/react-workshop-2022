198a86284e15eb939abf748411d2bc10d59d8c75
balazssk <balazs.safrany-kovalik@liferay.com>
2022-04-28 21:24:44 +0200

    WEEK-1 Finally understood what is the task

--- src/App.js
+++ src/App.js
@@ -6,20 +6,40 @@ import React from 'react';
 class App extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			eur: 0
+		};
 	}
 	
 	exchangeRate() {
       return Math.random() * 10000;
     }
+	
+	handleChange = (event) => {
+		this.setState((state, props) => ({
+			eur: event.target.value
+		}));
+	}
   
 	render() {
 		return (
 			<div className="App">
-				<Amount id="euros" name="Euros" value="2" />
-				<Amount id="btc" name="$BTC" readOnly={true} value={this.exchangeRate()} />
+				<Amount
+					id="euros"
+					className={this.state.eur < 0 ? "red" : ""}
+					handleChange={this.handleChange}
+					name="Euros"
+					value={this.state.eur} />
+				<Amount
+					id="btc"
+					name="$BTC"
+					readOnly={true}
+					value={this.state.eur * this.exchangeRate()} />
 			</div>
 		);
 	}
+	
+
 }
 
 export default App;
