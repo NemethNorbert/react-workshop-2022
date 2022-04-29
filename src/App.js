f984e8b2f1b9f5c540f3eaec6125cb5c900ac74e
Laszlo Pap <laszlo.pap@liferay.com>
2022-04-29 12:12:43 +0200

    Use React.createContext() to provide a dark/light theme toggle.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 const defaultEuroValue=10;
 const timeOutMillis=5000;
 const defaultBTCValue=0;
 var isRedBorderAdded = false;
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+var currentTheme = themes.light;
+
+const ThemeContext = React.createContext(currentTheme);
 
 
 function App() {
 
-	const [mystate, setMyState] = React.useState({euroValue: defaultEuroValue, btcValue: defaultBTCValue});
-    const {euroValue, btcValue} = mystate;
+	const [mystate, setMyState] = React.useState({euroValue: defaultEuroValue, btcValue: defaultBTCValue, currentTheme: themes.light});
+    const {euroValue, btcValue, currentTheme} = mystate;
     const exchangeRate = getExchangeRate();
 
 	useEffect(() => {
@@ -18,7 +31,8 @@ function App() {
 
 	    	setMyState({
 			  euroValue: euroValue,
-			  btcValue: defaultBTCValue
+			  btcValue: defaultBTCValue,
+			  currentTheme: currentTheme
 			});
 
 	    }, timeOutMillis);
@@ -31,22 +45,32 @@ function App() {
     const myOnChange = (element) => {
 		let calcEur = element.value;
 		let calcBtc = element.value / exchangeRate;
+		let theme=currentTheme;
+
+		if (theme==themes.light){
+			theme = themes.dark;
+		} else {
+			theme=themes.light;
+		}
 		
 		setMyState({
 		  euroValue: calcEur,
-		  btcValue: calcBtc
+		  btcValue: calcBtc,
+		  currentTheme: theme
 		});
 	}
 
-	const euroComponent = <Amount label="Euros" value={euroValue} onChange={myOnChange} isReadOnly={false} />
-	const btcComponent = <Amount label="BTC" value={btcValue} isReadOnly={true} />
+	const euroComponent =<ThemeContext.Provider value={currentTheme}><Amount label="Euros" value={euroValue} onChange={myOnChange} isReadOnly={false} /></ThemeContext.Provider>
+	const btcComponent = <ThemeContext.Provider value={currentTheme}><Amount label="BTC" value={btcValue} isReadOnly={true} /></ThemeContext.Provider>
 
   return (
-    React.createElement(React.Fragment,null,euroComponent,btcComponent)
+    	React.createElement(React.Fragment,null,euroComponent,btcComponent)
   );
 }
 
 function Amount({label, value, onChange, isReadOnly}) {
+
+	const theme = useContext(ThemeContext);
 
 	if (value >= 0){
     	isRedBorderAdded = false;
@@ -55,18 +79,22 @@ function Amount({label, value, onChange, isReadOnly}) {
     }
 
 	return (
-    <div className={(isRedBorderAdded && !isReadOnly) ? "error" : "App"}>
-    	<label>{label}:
-         <input type="number" value={value} readOnly={isReadOnly} onChange=
-	        {
-	        	(event) => {
-	        		onChange(event.target)
-	        	}
-	        }
-        >
-        </input>
-      </label>
-    </div>
+		<div style={{ background: theme.background, color: theme.foreground }}>
+		    <div className={(isRedBorderAdded && !isReadOnly) ? "error" : "App"}>
+		    	<label>{label}:
+		         <input type="number" value={value} readOnly={isReadOnly} onChange=
+			        {
+			        	(event) => {
+			        		onChange(event.target)
+			        	}
+			        }
+		        >
+		        </input>
+		      </label>
+		    </div>
+		</div>
+
+
   );
 }
 
