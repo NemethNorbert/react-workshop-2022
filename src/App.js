23f123bd348313593ce65522895ab4755f4e0247
laszlobereczki <laszlo.bereczki@liferay.com>
2022-04-29 12:05:28 +0200

    add amount function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    return (
+        <div className="App">
+        </div>
+    );
+}
+
+function Amount(props) {
+    const myStyle = (props.value<0) ? { color: "red" } : { color: "black" }
+    return <p style={myStyle}>{props.message}{props.value}</p>;
 }
 
 export default App;
