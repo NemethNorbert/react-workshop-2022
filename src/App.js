1f5f3ded393a5f5e7d64b4f610eaed4a888529be
Orsolya Dekany <orsolya.dekany@liferay.com>
2022-05-01 15:27:47 +0200

    feat(week1): Add a number input with a label, Euro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 function App() {
   return (
     <div className="App">
+      <label for="euro">Euro: </label>
+			<input id="euro" name="euro" placeholder="0" type="number" />
     </div>
   );
 }
