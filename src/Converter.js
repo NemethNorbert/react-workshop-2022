a9ddc68a4cb73391c088374d1635d546e7a293db
Vilmos Papp <vilmos.papp@liferay.com>
2022-05-04 14:30:07 +0200

    Added message handling

--- src/Converter.js
+++ src/Converter.js
@@ -1,10 +1,12 @@
 import React, { useContext }  from 'react';
 import Currency from './Currency.js';
-
-function Converter({convertedName, convertedLabel, exchangeRate}) {
+import './App.css';
+function Converter({convertedName, cryptoName, exchangeRate}) {
 
     const [value, setValue] = React.useState(0)
     const [convertedValue, setConvertedValue] = React.useState(0)
+    const [msg, setMsg] = React.useState('')
+    const [converts, setConverts] = React.useState(1)
 
     const validateValue = element => {
         if (element.value < 0) {
@@ -14,15 +16,26 @@ function Converter({convertedName, convertedLabel, exchangeRate}) {
           element.className = 'black'
         }
         setValue(element.value)
-        setConvertedValue(element.value * exchangeRate)
-    
       }
-    
 
+    function convert() {
+      increment()
+      setConvertedValue(value * exchangeRate)
+      if (converts > 4) {
+        setMsg('Convert without limits by becoming a premium user')
+      }
+    }
+
+    function increment() {
+      setConverts(converts + 1)
+      console.log(converts)
+    }
     return (
          <div>
+            <span className="premium" id="msg">{msg}</span>
             <Currency id="euro" name="Euro" value={value} className="black" readOnly={false} onChange={validateValue}/>
-            <Currency id={convertedName} name={convertedLabel} value={convertedValue} className="black" readOnly={true}/>
+            <Currency id={convertedName} name={cryptoName} value={convertedValue} className="black" readOnly={true} />
+            <button onClick={convert}>Convert</button>
          </div>
     )
 }
