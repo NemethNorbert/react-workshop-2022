1ca00ccfff09c512180114834701e3c538ebdc14
balazssk <balazs.safrany-kovalik@liferay.com>
2022-05-06 12:12:26 +0200

    WEEK-2 8 - Extract to Converter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Amount from './components/Amount';
+import Converter from './components/Converter';
 import React from 'react';
 
 
@@ -8,33 +8,14 @@ class App extends React.Component {
 	
 	constructor(props) {
 		super(props)
-		this.state = {
-			eur: 0
-		};
 		
 		this.exchangeRateBTC = 995;
-	}
-
-	handleChange = (event) => {
-		this.setState((state, props) => ({
-			eur: event.target.value
-		}));
 	}
   
 	render() {
 		return (
 			<div className="App">
-				<Amount
-					id="euros"
-					className={this.state.eur < 0 ? "red" : ""}
-					handleChange={this.handleChange}
-					name="Euros"
-					value={this.state.eur} />
-				<Amount
-					id="btc"
-					name="$BTC"
-					readOnly={true}
-					value={this.state.eur * this.exchangeRateBTC} />
+				<Converter cryptoName="$BTC" exchangeRate={this.exchangeRateBTC} />
 			</div>
 		);
 	}
---
+++ src/components/Converter.js
@@ -0,0 +1,42 @@
+import './Converter.css';
+import Amount from './Amount';
+import React from 'react';
+
+class Converter  extends React.Component {
+	prefix;
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			eur: 0
+		};
+		
+		this.prefix = props.cryptoName.toLowerCase().replace("$", "");
+	}
+	
+	handleChange = (event) => {
+		this.setState((state, props) => ({
+			eur: event.target.value
+		}));
+	}
+	
+	render() {
+		return (
+			<div className="converter">
+				<Amount
+					id={this.prefix + "euros"}
+					className={this.state.eur < 0 ? "red" : ""}
+					handleChange={this.handleChange}
+					name="Euros"
+					value={this.state.eur} />
+				<Amount
+					id={this.prefix}
+					name={this.props.cryptoName}
+					readOnly={true}
+					value={this.state.eur * this.props.exchangeRate} />
+			</div>
+		);
+	}
+}
+
+export default Converter;
