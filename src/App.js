51fa3c711d11be8d0b0030fd2e29bc2ce3cdf23f
Orsolya Dekany <orsolya.dekany@liferay.com>
2022-05-06 13:12:28 +0200

    SF

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-import Converter from './components/Converter';
-import {useEffect, useState} from 'react';
-import ThemeContext from './context/ThemeContext';
+import Converter from "./components/Converter";
+import { useEffect, useState } from "react";
+import ThemeContext from "./context/ThemeContext";
 
 function App() {
-  
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
   const [conversion, setConversion] = useState(0);
 
   const MAX_CONVERSION = 5;
 
   const countConversion = () => {
     setConversion(conversion + 1);
-  }
+  };
 
-  useEffect(()=>{
-    if (conversion > MAX_CONVERSION){
+  useEffect(() => {
+    if (conversion > MAX_CONVERSION) {
       alert("Convert without limits by becoming a premium user");
     }
-  },[conversion]);
+  }, [conversion]);
 
   return (
     <ThemeContext.Provider value={theme}>
       <div className={`app ${theme}`}>
         <select
-            onChange={event => setTheme(event.target.value)}
-            value={theme}
-            >
+          onChange={(event) => setTheme(event.target.value)}
+          value={theme}
+        >
           <option value="dark">Dark</option>
           <option value="light">Light</option>
         </select>
 
         <Converter
-            cryptoName="$BTC"
-            header={<h1>BTC converter</h1>}
-            exchangeRate={995}
-            onChange={countConversion}
+          cryptoName="$BTC"
+          header={<h1>BTC converter</h1>}
+          exchangeRate={995}
+          onChange={countConversion}
         />
         <Converter
-            cryptoName="$ETH"
-            header={<h1>ETH converter</h1>}
-            exchangeRate={1.2}
-            onChange={countConversion}
+          cryptoName="$ETH"
+          header={<h1>ETH converter</h1>}
+          exchangeRate={1.2}
+          onChange={countConversion}
         />
-     </div>
+      </div>
     </ThemeContext.Provider>
   );
 }
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,33 +1,28 @@
-import React, {useState } from 'react';
-import Amount from './Amount';
+import React, { useState } from "react";
+import Amount from "./Amount";
 
-function Converter({cryptoName, exchangeRate, header}) {
+function Converter({ cryptoName, exchangeRate, header }) {
+  const [value, setValue] = useState(0);
 
-    const [value, setValue] = useState(0);
+  const handleChange = (value) => {
+    setValue(value);
+  };
 
-    const handleChange = value => {
-		setValue(value);
-	}
-    
-    const cryptoValue = parseFloat(value * exchangeRate).toFixed(2);
+  const cryptoValue = parseFloat(value * exchangeRate).toFixed(2);
 
-    return (
-        <>
-            <div className="header">{header}</div>
-            <Amount 
-                name="Euro"                 
-                onChange={handleChange} 
-                readonly={false}
-                value={value}
-            />
-            
-            <Amount 
-                name={cryptoName}                 
-                readonly={true}
-                value={cryptoValue}
-            />         
-        </>
-    );
+  return (
+    <>
+      <div className="header">{header}</div>
+      <Amount
+        name="Euro"
+        onChange={handleChange}
+        readonly={false}
+        value={value}
+      />
+
+      <Amount name={cryptoName} readonly={true} value={cryptoValue} />
+    </>
+  );
 }
 
 export default Converter;
