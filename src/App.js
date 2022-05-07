bf4b016984e144c5f6a3cf78264afd931a75cc09
eszterdudasschveder <172996984@liferay.com>
2022-05-07 22:09:31 +0200

    Remove calcExchangeRate and timer

--- src/App.js
+++ src/App.js
@@ -3,28 +3,17 @@ import './App.css';
 import Amount from './components/Amount';
 import ThemeContext from './ThemeContext';
 
-const calcExchangeRate = () => {
-	return Math.random() * 10000;
-}
 
 function App() {
 	const [value, setValue] = useState(0);
-	const [exchangeRate, setExchangeRate] = useState(calcExchangeRate);
+	const [exchangeRate, setExchangeRate] = useState(995);
 	const [theme, setTheme] = useState("light");
-
-	useEffect(() => {
-		let timer = setTimeout(()=>{setExchangeRate(0)}, 5000);
-
-		return () => {
-			clearTimeout(timer);
-		}
-	},[value]);
 
 	const onChange = (value) => {
 		setValue(value);
 
 		if (exchangeRate === 0) {
-			setExchangeRate(calcExchangeRate);
+			setExchangeRate(995);
 		}
 	}
 
