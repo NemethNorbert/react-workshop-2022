64c711dcaf81630e658bc76945f894ec901285b0
Eva Budai <eva.budai@liferay.com>
2022-05-08 19:05:59 +0200

    step 6 - Set timer to crash the BTC price

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import Amount from './components/Amount';
 
@@ -9,21 +7,33 @@ const convertToBtc = () => {
 }
 
 function App() {
-  const [amount, setAmount] = useState(
-      () => window.localStorage.getItem('name') || 0,
-  )
-  useEffect(() => {
-       window.localStorage.setItem('name', amount)
-  }, [amount])
+    const [amount, setAmount] = useState(
+        () => window.localStorage.getItem('name') || 0,
+    )
+    const [exchangeRate, setExchangeRate] = useState(convertToBtc);
 
-  const [exchangeRate, setExchangeRate] = useState(convertToBtc);
+    useEffect(() => {
+        window.localStorage.setItem('name', amount);
+        let timer = setTimeout(()=>{setExchangeRate(0)}, 5000);
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [amount])
 
-  return (
-    <div className="App">
-      <Amount currency="Euro" value={amount} onChange={setAmount} />
-      <Amount currency="BTC" value={amount * exchangeRate} />
-    </div>
-  );
+    const onChange = (value) => {
+        setAmount(value);
+
+        if (exchangeRate === 0) {
+            setExchangeRate(convertToBtc);
+        }
+    }
+
+    return (
+        <div className="App">
+            <Amount currency="Euro" value={amount} onChange={onChange}/>
+            <Amount currency="BTC" value={amount * exchangeRate}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import '../amount.css';
 
 function Amount({currency, value, onChange}) {
