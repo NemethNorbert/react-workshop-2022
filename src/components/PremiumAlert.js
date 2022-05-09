ddcc3494e4bf604028450c4caa97d6bfa95bdabd
rolandpakai <roland.pakai@liferay.com>
2022-05-09 16:59:18 +0200

    Week3-Task11: Add Premium conversion message

--- src/components/PremiumAlert.js
+++ src/components/PremiumAlert.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { usePremium } from './PremiumContext';
 
 const PremiumAlert = () => {
-    const premiumCount = usePremium();
-    
-    return (
-        <>
-          {premiumCount > 4 &&
-            <div className="premium-alert center">
-                <div className="alert alert-warning">
-                  <div className="alert-container">
-                    Convert without limits by becoming a premium user
-                  </div>
-                </div>
-            </div>
-          }
-        </>
-    );
+  const [premium, setPremium] = useState(false);
+  const premiumCount = usePremium();
+
+  const onClick = () => {
+    setPremium(prevPremium => !prevPremium);
   }
+    
+  return (
+      <>
+        {!premium && premiumCount > 4 &&
+          <div className="premium-alert center">
+              <div className="alert alert-warning">
+                <div className="alert-container">
+                  <button className="premium-alert-button center" type="button" onClick={onClick}> 
+                    Convert without limits by becoming a Premium user
+                  </button>
+                </div>
+              </div>
+          </div>
+        }
+        {premium &&
+          <div className="premium-alert center">
+            💎 P R E M I U M
+          </div>
+        }
+      </>
+  );
+}
   
   export default PremiumAlert;
