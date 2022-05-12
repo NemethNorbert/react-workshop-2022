2ace358699825f9b0f9245d530959dba50c94a76
balazssk <balazs.safrany-kovalik@liferay.com>
2022-05-12 12:19:32 +0200

    WEEK-3 11 - Implement a Become Premium button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Converter from "./components/Converter";
+import BecomePremium from "./components/BecomePremium";
 import React from "react";
 
 
@@ -9,27 +10,42 @@ class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			conversions: 0
+			conversions: 0,
+			isPremium: false
 		};
 
 		this.exchangeRateBTC = 995;
 	}
-	
+
+	onBecomePremium = (event) => {
+		event.preventDefault();
+		
+		this.setState({
+			isPremium: true
+		});
+	}
+
 	onConversion = () => {
 		this.setState((state, props) => ({
 			conversions: state.conversions + 1
 		}));
 	}
-	
+
 	componentDidUpdate() { 
-		if (this.state.conversions == 5 ) {
+		if (!this.state.isPremium && this.state.conversions == 5 ) {
 			console.log("Need more conversions");
 		}
 	};
-  
+
 	render() {
 		return (
 			<div className="App">
+				{!this.state.isPremium ?
+					<BecomePremium
+						onClick={this.onBecomePremium}
+					/>
+					: ""
+				}
 				<Converter
 					onConversion={this.onConversion}
 					cryptoName="$BTC"
